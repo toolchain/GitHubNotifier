8e4d2da8586fadcccfe50d9e48abf68de878930c
Oleksii Karavaiev <oleksii.karavaiev@gmail.com>
2011-12-01 11:25:35 +0200

    Main form created. Basic features added.

---
+++ GitHubNotifier/WindowsFormsApplication1/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIconGit = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIconGit
+            // 
+            this.notifyIconGit.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconGit.Icon")));
+            this.notifyIconGit.Text = "GitHub Notifier";
+            this.notifyIconGit.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIconGit_MouseDoubleClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 127);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "GitHub Notifier";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconGit;
+    }
+}
+
---
+++ GitHubNotifier/WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.SetFormPosition();
+
+            this.notifyIconGit.Visible = true;
+            this.ShowInTaskbar = false;
+        }
+
+        /// <summary>
+        /// Restore form from system tray
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void notifyIconGit_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+
+            this.WindowState = FormWindowState.Normal;
+            this.notifyIconGit.Visible = false;
+        }
+
+        /// <summary>
+        /// Hide form to system tray
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            this.notifyIconGit.Visible = true;
+            this.ShowInTaskbar = false;
+        }
+
+        /// <summary>
+        /// Set form position to the right bottom corner of the screen
+        /// </summary>
+        private void SetFormPosition()
+        {
+            int x = Screen.PrimaryScreen.WorkingArea.Width - this.Width;
+            int y = Screen.PrimaryScreen.WorkingArea.Height - this.Height;
+            this.Location = new Point(x, y);
+        }
+
+        /// <summary>
+        /// Prevent user from moving form
+        /// </summary>
+        /// <param name="m"></param>
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_NCLBUTTONDOWN = 161;
+            const int WM_SYSCOMMAND = 274;
+            const int HTCAPTION = 2;
+            const int SC_MOVE = 61456;
+            if ((m.Msg == WM_SYSCOMMAND) && (m.WParam.ToInt32() == SC_MOVE))
+            {
+                return;
+            }
+
+            if ((m.Msg == WM_NCLBUTTONDOWN) && (m.WParam.ToInt32() == HTCAPTION))
+            {
+                return;
+            }
+
+            base.WndProc(ref m);
+        }
+    }
+}
