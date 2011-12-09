9c2d4c186b64710368eb9455542fdd225324f560
Oleksii Karavaiev <oleksii.karavaiev@gmail.com>
2011-12-09 13:13:20 +0200

    Options dialog added

---
+++ GitHubNotifier/WindowsFormsApplication1/Options.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Drawing;
+
+namespace GitHubNotifier
+{
+    public class Options
+    {
+        private const string PathSection = "PATHS";
+        private const string GeneralSection = "GENERAL";
+        private const string PathToFeedKey = "PathToFeed";
+        private const string UpdateIntervalKey = "UpdateInterval";
+
+        private const int DefaultUpdateInterval = 30000;
+
+        private string filePath;
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        public Options(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        private void Write(string section, string key, string value)
+        {
+            WritePrivateProfileString(section, key, value.ToLower(), this.filePath);
+        }
+
+        private string Read(string section, string key)
+        {
+            StringBuilder SB = new StringBuilder(255);
+            int i = GetPrivateProfileString(section, key, String.Empty, SB, 255, this.filePath);
+            return SB.ToString();
+        }
+
+        public string FilePath
+        {
+            get { return this.filePath; }
+            set { this.filePath = value; }
+        }
+
+        public string PathToFeed;
+        public int UpdateInterval = DefaultUpdateInterval;
+
+        /// <summary>
+        /// Reads options from INI file
+        /// </summary>
+        public void ReadOptions()
+        {
+            string str = string.Empty;
+
+            // read paths section
+            this.PathToFeed = Read(PathSection, PathToFeedKey);
+            str = Read(GeneralSection, UpdateIntervalKey);
+            
+            if (string.Empty != str)
+                this.UpdateInterval = Convert.ToInt32(str);
+        }
+
+        /// <summary>
+        /// Writes options to INI file
+        /// </summary>
+        public void WriteOptions()
+        {
+            Write(PathSection, PathToFeedKey, this.PathToFeed);
+            Write(GeneralSection, UpdateIntervalKey, this.UpdateInterval.ToString());
+        }
+    }
+}
+
---
+++ GitHubNotifier/WindowsFormsApplication1/OptionsDialog.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace GitHubNotifier
+{
+    partial class OptionsDialog
+    {
+        /// <summary>
+        /// Form constructor. Initializing options values
+        /// </summary>
+        public OptionsDialog(Options programOptions)
+        {
+            InitializeComponent();
+            this.textBoxPathToFeed.Text = programOptions.PathToFeed;
+            this.textBoxUpdateInterval.Text = programOptions.UpdateInterval.ToString();
+        }
+
+        public string GetPathToFeed()
+        {
+            return this.textBoxPathToFeed.Text;
+        }
+
+        public string GetUpdateInterval()
+        {
+            return this.textBoxUpdateInterval.Text;
+        }
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.textBoxPathToFeed = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUpdateInterval = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(297, 210);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 0;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // textBoxPathToFeed
+            // 
+            this.textBoxPathToFeed.Location = new System.Drawing.Point(100, 12);
+            this.textBoxPathToFeed.Name = "textBoxPathToFeed";
+            this.textBoxPathToFeed.Size = new System.Drawing.Size(272, 20);
+            this.textBoxPathToFeed.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path to feed:";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(216, 210);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Update interval:";
+            // 
+            // textBoxUpdateInterval
+            // 
+            this.textBoxUpdateInterval.Location = new System.Drawing.Point(100, 38);
+            this.textBoxUpdateInterval.Name = "textBoxUpdateInterval";
+            this.textBoxUpdateInterval.Size = new System.Drawing.Size(57, 20);
+            this.textBoxUpdateInterval.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ms";
+            // 
+            // OptionsDialog
+            // 
+            this.AcceptButton = this.buttonOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(384, 245);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxUpdateInterval);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPathToFeed);
+            this.Controls.Add(this.buttonOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "OptionsDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "GitHub Notifier Options";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.TextBox textBoxPathToFeed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUpdateInterval;
+        private System.Windows.Forms.Label label3;
+    }
+}
